995984186a12d2807b9896d4c5be9203f0c409b6
Vladyslav Shestakov <vlshestakoff@gmail.com>
2025-03-23 23:02:09 +0200

    Update script.js

--- homework_7.1_7.2_7.3/script.js
+++ homework_7.1_7.2_7.3/script.js
@@ -1,17 +1,17 @@
 // 7.1
 // функція із замиканням
-function getSum(num) {
+function getSum() {
     let result = 0;
-    return function sumInner() {
+    return function(num) {
         return result += num;
     }
 }
 
-let sum = getSum(4);
-console.log(sum()); // 4
-console.log(sum()); // 8
-console.log(sum()); // 12
-
+let sum = getSum();
+console.log(sum(4)); // 4
+console.log(sum(6)); // 8
+console.log(sum(10)); // 12
+console.log(sum(7));
 
 
 // 7.2 
@@ -36,14 +36,17 @@ function showUserData() {
     for (let i = 0; i < 10; i ++) {
         userData = prompt('Введіть число більше 100', '');
 
-        if (isNaN(userData) || userData >= 100) { // перевірка на те, чи є букви в userData. Якщо є - userData буде NaN
+        if (userData !== null) { // щоб не клацати 10 разів, якщо юзер натиснув скасувати. Виведе все одно останнє введене значення
             result = userData;
+        } else {
+            break;
+        }
+
+        if (isNaN(userData) || userData >= 100) { // перевірка на те, чи є букви в userData. Якщо є - userData буде NaN
                 break;
             }
 
-        if (userData < 100 && i === 9) {
-                result = userData;
-        } else {
+        if (userData < 100 && i < 9) {
                 alert('Введіть ще раз');
             }
         }
