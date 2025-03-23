2c2b294cecb7288867153f2e118d707fa9e809d1
Vladyslav Shestakov <vlshestakoff@gmail.com>
2025-03-23 22:43:38 +0200

    Create script.js

---
+++ homework_7.1_7.2_7.3/script.js
@@ -0,0 +1,54 @@
+// 7.1
+// функція із замиканням
+function getSum(num) {
+    let result = 0;
+    return function sumInner() {
+        return result += num;
+    }
+}
+
+let sum = getSum(4);
+console.log(sum()); // 4
+console.log(sum()); // 8
+console.log(sum()); // 12
+
+
+
+// 7.2 
+// Добуток через карування
+
+function getMultiply(a) {
+    return function(b) {
+        return a * b;
+    }
+}
+
+console.log(getMultiply(4)(5)); // 20
+
+
+// 7.3 
+// Функція з циклом
+
+function showUserData() {
+    let result = '';
+    let userData;
+
+    for (let i = 0; i < 10; i ++) {
+        userData = prompt('Введіть число більше 100', '');
+
+        if (isNaN(userData) || userData >= 100) { // перевірка на те, чи є букви в userData. Якщо є - userData буде NaN
+            result = userData;
+                break;
+            }
+
+        if (userData < 100 && i === 9) {
+                result = userData;
+        } else {
+                alert('Введіть ще раз');
+            }
+        }
+
+        return console.log(result);
+    }
+
+showUserData();
